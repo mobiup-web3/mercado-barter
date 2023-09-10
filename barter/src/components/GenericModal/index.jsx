00f123d19e31f88346cc7dde24fe526434f1758b
Antonio Carlos <antonio.carlos@mobiup.com.br>
2023-09-10 11:41:50 -0300

    Adicionado spinner de loading no envio da oferta

--- barter/src/components/GenericModal/index.jsx
+++ barter/src/components/GenericModal/index.jsx
@@ -3,6 +3,7 @@ import Modal from 'react-bootstrap/Modal';
 
 const GenericModal = ({
   show,
+  showHeader,
   handleClose,
   title,
   children,
@@ -14,10 +15,12 @@ const GenericModal = ({
 
   return (
     <Modal show={show} onHide={handleClose} centered={centered} backdrop={backdrop} keyboard={keyboard} size={size}>
-        <div className="modal-header border-0 d-flex align-items-center">
-          <h3 className="m-0 fw-semibold">{title}</h3>
-          <button type="button" className="btn-close" aria-label="Close" onClick={handleClose}></button>
-        </div>
+        {showHeader && (
+          <div className="modal-header border-0 d-flex align-items-center">
+            <h3 className="m-0 fw-semibold">{title}</h3>
+            <button type="button" className="btn-close" aria-label="Close" onClick={handleClose}></button>
+          </div>
+        )}
         <div className="modal-body py-4 pt-0 d-grid gap-3 rounded-bottom">
           {children}
         </div>
